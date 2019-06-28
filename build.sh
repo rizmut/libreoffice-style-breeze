09807a772d13060109722f4251d7cb912ee33d06
rizmut <riz_17_oke@yahoo.co.id>
2019-06-29 01:11:36 +0700

    Fix for tdf#124127, tdf#89884

---
+++ build.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+echo "=> Remove old PNG both light and dark version"
+cp "images_breeze/links.txt" \
+   "images_breeze_svg"
+rm -Rf "images_breeze"
+rm -Rf "images_breeze_dark"
+
+cp -Rf "images_breeze_svg" \
+   "images_breeze"
+rm "images_breeze_svg/links.txt"
+cd "images_breeze"
+
+echo "=> Export light SVG to PNG ..."
+find -name "*.svg" -o -name "*.SVG" | while read i;
+do 
+	echo "This $i file is compressed"
+	fname=$( basename "$i")
+#	echo "has the name: $fname"
+	fdir=$( dirname "$i")
+#	echo "and is in the directory: ${fdir##*/}"
+	inkscape -f "$i" -e "${i%.*}.png"
+	optipng -o7 "${i%.*}.png"
+	#convert "$i" -quality 75 "$i"
+done
+
+echo "=> Delete light SVG files ..."
+find -name "*.svg" -o -name "*.SVG" | while read i;
+do
+    fname=$( basename "$i")
+    fdir=$( dirname "$i")
+    rm "$i"
+done
+
+cd "./.."
+
+cp -Rf "images_breeze_svg" \
+   "images_breeze_dark"
+cd "images_breeze_dark"   
+
+find -name "*.svg" -o -name "*.SVG" | while read i;
+do 
+	echo "This $i file will be Dark Theme"
+	fname=$( basename "$i")
+#	echo "has the name: $fname"
+	fdir=$( dirname "$i")
+#	echo "and is in the directory: ${fdir##*/}"
+	#inkscape -f "$i" -e "${i%.*}.png"
+	sed -i -e 's/#4d4d4d/#f2f2f2/g' "$i"
+	#convert "$i" -quality 75 "$i"
+done 
+
+echo "=> Export dark SVG to PNG ..."
+find -name "*.svg" -o -name "*.SVG" | while read i;
+do 
+	echo "This $i file is compressed"
+	fname=$( basename "$i")
+#	echo "has the name: $fname"
+	fdir=$( dirname "$i")
+#	echo "and is in the directory: ${fdir##*/}"
+	inkscape -f "$i" -e "${i%.*}.png"
+	optipng -o7 "${i%.*}.png"
+	#convert "$i" -quality 75 "$i"
+done 
+
+echo "=> Delete dark SVG files ..."
+find -name "*.svg" -o -name "*.SVG" | while read i;
+do
+    fname=$( basename "$i")
+    fdir=$( dirname "$i")
+    rm "$i"
+done
